7d95335c067f8a9c67f551752cdf39638f2b7da5
Ankit Salian <ankitsalian9@gmail.com>
2020-09-09 09:45:51 +0530

    Configured nav bar of Amazon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header'
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
