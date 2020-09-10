af5dde456ed3d440ce13f758b39e614f55172ad2
Ankit Salian <ankitsalian9@gmail.com>
2020-09-10 15:41:24 +0530

    Modified checkout page and added firebase json

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,14 +5,15 @@ import './Checkout.css';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
-    
+    const [{ basket, user }, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._C423492668_.jpg" alt="Checkout banner" />
 
                 <div className="checkout__title">
+                    <h3>Hello, {`${user ? user.email + ' ' : ''}`}</h3>
                     <h2>Your Shopping Basket</h2>
                     {basket.map(item => (
                         <CheckoutProduct key={`checkout_${item.id}`}
